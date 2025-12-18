ecbee1a655ee0c9e0e1cbfc236b5fa8c41b17aeb
Harsh20056 <shrivastavah896@gmail.com>
2025-12-18 14:04:02 +0530

    To-do list

---
+++ script.js
@@ -0,0 +1,47 @@
+let input = document.querySelector("input");
+let container = document.querySelector(".container");
+
+let addBtn = document.querySelector("#add");
+
+let tasksArr = JSON.parse(localStorage.getItem("items")) || [];
+console.log(tasksArr);
+
+const renderUi = () => {
+  container.innerHTML = "";
+  tasksArr.forEach((val, index) => {
+    container.innerHTML += `
+  <div class="list-conatiner">
+        <p>${val}</p>
+        <div class="btns">
+          <button onclick="handleUpdate(${index})">Update</button>
+          <button onclick="handleDel(${index})" id="del">Delete</button>
+        </div>
+      </div>`;
+  });
+};
+
+renderUi();
+
+addBtn.addEventListener("click", (e) => {
+  let inpValue = input.value;
+
+  tasksArr.push(inpValue);
+  localStorage.setItem("items", JSON.stringify(tasksArr));
+  renderUi();
+
+  input.value = "";
+});
+
+const handleDel = (index) => {
+  tasksArr.splice(index, 1);
+  localStorage.setItem("items", JSON.stringify(tasksArr));
+  renderUi();
+};
+
+const handleUpdate = (index) => {
+  let updateValue = prompt("Enter new value", tasksArr[index]);
+  tasksArr[index] = updateValue;
+  localStorage.setItem("items", JSON.stringify(tasksArr));
+  renderUi();
+};
+
